8a4e5925c44bbd9030c9a9dbdfceab34c26d2b86
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-08 00:57:34 +0600

    reference-error

---
+++ reference-error.js
@@ -0,0 +1,22 @@
+const a = "a";
+const b = "b";
+// console.log(z);
+
+// console.log(myName);
+const myName = "Touhid";
+
+console.log(add(2, 3));
+
+function add(a, b) {
+	return a + b;
+}
+
+// console.log(add2(5, 2));
+
+const add2 = (a, b) => a + b;
+
+function doSomething() {
+	const sum = "add";
+}
+
+// console.log(sum);
