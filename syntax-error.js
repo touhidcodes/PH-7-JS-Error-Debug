301ece12add506e15aea654836024f2f0b1cf70c
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-08 00:24:22 +0600

    syntax-error

---
+++ syntax-error.js
@@ -0,0 +1,36 @@
+function isBig(num) {
+	if (num > 5) {
+		return true;
+	} else {
+		return false;
+	}
+}
+// }
+
+const big = isBig(5);
+
+// const sum = a, b => a + b;
+const sum = (a, b) => a + b;
+
+const mask = {
+	name: "elon",
+	// age: 53
+	// year: 23
+	age: 53,
+	year: 23,
+};
+
+// const number = [23, 45, 34 45, 56];
+const number = [23, 45, 34, 45, 56];
+
+// for (let i = 0; i < array.length i++) {
+for (let i = 0; i < array.length; i++) {
+	// const element = array[i;
+	const element = array[i];
+}
+
+// console.log(num;
+
+// if (condition) && (condition) {
+if (condition && condition) {
+}
